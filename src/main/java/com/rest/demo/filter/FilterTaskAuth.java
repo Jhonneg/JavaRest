0f3cccf2142938d4e7b9cfb39185b74cae136bd3
Jhonne <jonee4339@hotmail.com>
2026-06-07 14:07:50 -0300

    added filtertask auth

--- src/main/java/com/rest/demo/filter/FilterTaskAuth.java
+++ src/main/java/com/rest/demo/filter/FilterTaskAuth.java
@@ -3,9 +3,13 @@ package com.rest.demo.filter;
 import java.io.IOException;
 import java.util.Base64;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import com.rest.demo.user.IUserRepository;
+
+import at.favre.lib.crypto.bcrypt.BCrypt;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -14,21 +18,46 @@ import jakarta.servlet.http.HttpServletResponse;
 @Component
 public class FilterTaskAuth extends OncePerRequestFilter {
 
+    @Autowired
+    private IUserRepository userRepository;
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
         var authorization = request.getHeader("Authorization");
+
         var authEncoded = authorization.substring("Basic".length()).trim();
+
         byte[] authEncode = Base64.getDecoder().decode(authEncoded);
+
         var authString = new String(authEncode);
+
         String[] credentials = authString.split(":");
         String username = credentials[0];
         String password = credentials[1];
+
+        System.out.println("Authorization");
+        System.out.println(username);
+        System.out.println(password);
+        
+        var user = this.userRepository.findByUsername(username);
+        if (user == null) {
+            response.sendError(401);
+        } else {
+            var passwordVerify = BCrypt.verifyer().verify(password.toCharArray(), user.getPassword());
+
+            if (passwordVerify.verified) {
+                filterChain.doFilter(request, response);
+
+            } else {
+                response.sendError(401);
+
+            }
+        }
         System.out.println("Authorization");
         System.out.println(username);
         System.out.println(password);
 
-        filterChain.doFilter(request, response);
     }
 
 }
